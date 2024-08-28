f5a87333ac0bbfeed404084e49ef12ba9fa455cc
tetik-mehmet <tetikmehmet930@gmail.com>
2024-08-28 16:10:52 +0300

    en küçük ve en büyük hesaplandı

--- index.js
+++ index.js
@@ -99,7 +99,17 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+enkucuk = sayilar[0];
+enbuyuk = sayilar[0];
+for(let i = 0; i<sayilar.length; i++){
+  if(sayilar[i] < enkucuk){
+    enkucuk = sayilar[i];
+  }
+  if(sayilar[i] > enbuyuk){
+    enbuyuk = sayilar[i];
+  }
+}
+
 
 // 3b çözümü:
 
